6e1a7c5729beb82d3b22de2667c016eebdbd3183
ahmade-shehadeh <ahmade.shehadeh@gmail.com>
2023-03-14 21:56:30 +0300

    _Project_4 frontend amount in cart

--- frontend/src/components/Cart/Cart.js
+++ frontend/src/components/Cart/Cart.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState,useContext } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import axios from "axios";
 import { Button, Card } from "react-bootstrap";
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
@@ -6,15 +6,17 @@ import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined
 import AddCircleOutlineOutlinedIcon from "@mui/icons-material/AddCircleOutlineOutlined";
 import ArrowBackOutlinedIcon from "@mui/icons-material/ArrowBackOutlined";
 import { UserContext } from "../../App";
-import './Cart.css'
+import "./Cart.css";
 //!=====================
 
 //!=============
 const Cart = () => {
- const {setCartL}=useContext(UserContext)
+  const { setCartL } = useContext(UserContext);
   const [cart, setCart] = useState("");
-  const [total, setTotal] = useState(0)
-  const [price, setPrice] = useState(0)
+  const [total, setTotal] = useState(0);
+  const [price, setPrice] = useState(0);
+  const [amount, setAmount] = useState(1);
+  const [amount_id, setAmount_id] = useState(1);
   const token = localStorage.getItem("token");
   //todo function DeleteproductCart
   const DeleteproductCart = (_id) => {
@@ -32,8 +34,32 @@ const Cart = () => {
         });
         console.log("in Deleteproduct:", newarrproduct);
         console.log(cart);
-        console.log('res',res);
+        console.log("res", res);
         setCart(newarrproduct);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  //todo function updateproductCart
+  const updateproductCart = (_id,newamount) => {
+    
+    console.log("newamount:", newamount);
+    axios
+      .put(
+        `http://localhost:5000/product/updat/${_id}`,
+        { amount: newamount },
+        {
+          headers: {
+            authorization: `Bearer ${token}`,
+          },
+        }
+      )
+      .then((res) => {
+        console.log(res.data.amount);
+        const newprice = parseInt(res.data.price, 10) * newamount;
+        console.log("newprice: ", newprice);
+        Total(newprice);
       })
       .catch((err) => {
         console.log(err);
@@ -56,47 +82,78 @@ const Cart = () => {
       .catch((err) => {
         console.log(err);
       });
-  }, []);
-  const Total=(price =0)=>{
-  let Totalprice=
-    cart&& cart.reduce((acc, elem) =>{
-      let price =parseInt( elem.product.price,10);
-      console.log(price);
-      
-    return acc + price
-    },0)
-    Totalprice =Totalprice+price
-    // setTotal(Totalprice)
-    return Totalprice
-  }
- 
 
-console.log('cart.length',cart.length);
-setCartL(cart.length)
+    cart && Total();
+  }, []);
+  const Total = (price = 0) => {
+    price = parseInt(price, 10);
+    console.log("price1: ", price);
+
+    let Totalprice =
+      cart &&
+      cart.reduce((acc, elem) => {
+        let price2 = parseInt(elem.product.price, 10);
+        console.log("price2: ", price);
+
+        return acc + price2;
+      }, price);
+    Totalprice = Totalprice + price;
+    console.log("Totalprice: ", Totalprice);
+    setTotal(Totalprice);
+    // return Totalprice
+  };
+
+  console.log("cart.length", cart.length);
+  setCartL(cart.length);
   return (
     <div>
-      <div className="total"><p>Total : {Total()}$</p></div>
+      <div className="total">
+        <p>Total : {total}$</p>
+      </div>
       <div className="product">
-        {console.log("cart", cart)}
-
         {cart.length > 0 ? (
           cart &&
-          cart.map((elem,i) => {
-            // let newprice=elem.product.price;
-          
-         
-          
-          
+          cart.map((elem, i) => {
             return (
               <Card key={elem.product._id} className="text-center">
                 <Card.Header>{elem.product.kind}</Card.Header>
+
                 <Card.Body className="bodycard">
                   <Card.Title>{elem.product.name}</Card.Title>
                   <Card.Text>
                     {elem.product.description}
                     <p> manufacturingyear: {elem.product.manufacturingyear}</p>
-                    <p> price: {elem.product.price}$</p>
-                    
+                    <div className="price">
+                      <button
+                        className="btnprice"
+                        onClick={() => {
+                          const newamount=elem.product.amount +1
+                          setAmount(newamount)
+                          setAmount_id(elem.product._id)
+                          updateproductCart(elem.product._id,newamount);
+                        }}
+                      >
+                        +
+                      </button>
+                      <p className="pPrice">
+                        {" "}
+                        price:{" "}
+                        { parseInt(elem.product.price, 10) * elem.product.amount }
+                        $
+                      </p>
+
+                      <button
+                        className="btnprice"
+                        onClick={() => {
+                          Total(elem.product.price);
+                          console.log("_id:", elem.product._id);
+                          setAmount(elem.product.amount - 1);
+                        }}
+                      >
+                        -
+                      </button>
+                    </div>
+                    <p>amount:{elem.product.amount}</p>
                   </Card.Text>
                   <Button variant="outline-success">buying</Button>
                   <Button
@@ -107,10 +164,6 @@ setCartL(cart.length)
                   >
                     <DeleteOutlineOutlinedIcon />
                   </Button>
-                  <Button variant="secondary" onClick={()=>{
-                    Total(elem.product.price)
-                    
-                  }}>+</Button>
                 </Card.Body>
                 <Card.Footer className="text-muted">
                   delivery: {elem.product.delivery} state: {elem.product.state}
@@ -127,7 +180,6 @@ setCartL(cart.length)
           <></>
         )}
       </div>
-      
     </div>
   );
 };
@@ -145,3 +197,12 @@ export default Cart;
 //               );
 //             setDescription(elem.product.description);
 //             setImg(elem.product.img);
+
+// if (price) {
+// }else{
+//       let Totalprice=60
+//       Totalprice =Totalprice+price
+//       console.log('Totalprice: ',Totalprice);
+//       // setTotal(Totalprice)
+//       return Totalprice
+//     }
